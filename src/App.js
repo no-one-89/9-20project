f132a783b5dc7f2a7444c3613923ff83fd463295
no-one-89 <67049155@qq.com>
2016-09-20 14:54:00 +0800

    add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import NavHeader from './components/NavHeader';
+import NavFooter from './components/NavFooter';
+import './main.css';
 
 class App extends React.Component {
   render () {
     return(
-      <div>
-          APP页面
-          {this.props.children}
+      <div className="mycontainer">
+          <NavHeader />
+          <div className="content-main">
+            {this.props.children}
+          </div>
+          <NavFooter />
       </div>
     )
   }
