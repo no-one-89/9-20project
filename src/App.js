88dddf59b5c8f1982c4cdedc3c6089b5a45d60b2
no-one-89 <67049155@qq.com>
2016-09-21 08:28:45 +0800

    add footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import NavHeader from './components/NavHeader';
 import NavFooter from './components/NavFooter';
+import LeftNav from './components/LeftNav';
 import './main.css';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      showNav:false
+    }
+  }
+  changeNavbar(){
+    this.setState({
+      showNav:window.innerWidth > 760 ? false : true
+    })
+  }
+  componentDidMount(){
+   this.changeNavbar();
+   window.onresize = this.changeNavbar.bind(this)
+  }
   render () {
     return(
       <div className="mycontainer">
-          <NavHeader />
+        {this.state.showNav ? <NavHeader /> : <LeftNav />}
           <div className="content-main">
             {this.props.children}
           </div>
-          <NavFooter />
+        {this.state.showNav ? <NavFooter /> : null}
+
       </div>
     )
   }
