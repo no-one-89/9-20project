d97c3af1e4281e8d3e43afb5ed9f96816abfa332
no-one-89 <67049155@qq.com>
2016-09-23 09:39:15 +0800

    del src

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Routers from './Routers';
-import 'highlight.js/styles/github.css'
-
-render(<Routers />,document.getElementById('root'));
