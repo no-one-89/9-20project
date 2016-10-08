f3dab74528ae797181168c89f1a7427b49a51d15
no-one-89 <67049155@qq.com>
2016-10-08 10:54:43 +0800

    finish

--- src/Blog/BlogItem.js
+++ src/Blog/BlogItem.js
@@ -2,6 +2,8 @@ import React, { PropTypes } from 'react'
 import { getBlogContent } from '../utils/helps'
 import marked from 'marked';
 import hljs from 'highlight.js';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import CircularProgress from '../components/CircularProgress'
 
 class BlogItem extends React.Component {
   constructor(){
@@ -22,16 +24,28 @@ class BlogItem extends React.Component {
       })
   }
   render () {
-    let content = this.state.wait ? '请稍等' : marked(this.state.data)
+    let progress = (
+      <div className="progress">
+        <MuiThemeProvider>
+          <CircularProgress />
+        </MuiThemeProvider>
+    </div>
+    )
+    let content =  marked(this.state.data)
     marked.setOptions({
     highlight: function (code) {
         return hljs.highlightAuto(code).value;
       }
     });
     return(
-      <div style={{width:"90%",margin:"0 auto"}}>
-         <div dangerouslySetInnerHTML={{__html: content}} />
+      <div style={{width:"100%",height:"100%"}}>
+        {this.state.wait ? progress : (<div style={{width:"90%",margin:"0 auto"}}>
+                                        <div dangerouslySetInnerHTML={{__html: content}} />
+                                      </div>)}
       </div>
+
+
+
     )
   }
 }
--- src/Work/Work.js
+++ src/Work/Work.js
@@ -12,11 +12,11 @@ class Work extends React.Component {
         title={'作品二：魔赞'}
         date={'2016/08/10'}
         href={'http://morethan.cc/index.php?c=index&a=index'} />
-        <WorkCard src={"https://github.com/no-one-89/9-20project--template/blob/master/src/images/PC.jpg?raw=true"}
+        <WorkCard src={"https://github.com/no-one-89/9-20project--template/blob/master/sources/codester.png?raw=true"}
         title={'作品三：Codester'}
         date={'2016/07/09'}
         href={'#'} />
-        <WorkCard src={"https://github.com/no-one-89/9-20project--template/blob/master/src/images/EagleTroop.png?raw=true"}
+        <WorkCard src={"https://github.com/no-one-89/9-20project--template/blob/master/sources/et.png?raw=true"}
         title={'作品四：EagleTroop'}
         date={'2016/06/21'}
         href={'#'} />
